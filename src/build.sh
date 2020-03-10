4061a517cab9741c6b49481c570384c8ffd035e4
Lars Thorup <lars@zealake.com>
2020-03-10 16:27:03 +0100

    clean up test container

--- src/build.sh
+++ src/build.sh
@@ -1,1 +1,2 @@
 docker build -t larsthorup-k8s-kind-sandbox/hello:local .
+docker image ls larsthorup-k8s-kind-sandbox/hello:local
--- src/test.sh
+++ src/test.sh
@@ -1,3 +1,5 @@
-docker run -p 49160:8080 -d larsthorup-k8s-kind-sandbox/hello:local
+docker run -p 49160:8080 -d larsthorup-k8s-kind-sandbox/hello:local --name hello
 sleep 10
 curl localhost:49160 | grep "Hello World"
+docker container stop hello
+docker container rm hello
