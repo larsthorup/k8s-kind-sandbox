799b49d2d0923cbfc066cf596142562821907d7d
Lars Thorup <lars@zealake.com>
2020-03-10 16:35:52 +0100

    fix docker syntax

--- src/test.sh
+++ src/test.sh
@@ -1,4 +1,4 @@
-docker run -p 49160:8080 -d larsthorup-k8s-kind-sandbox/hello:local --name hello
+docker run --name hello --publish 49160:8080 --detach larsthorup-k8s-kind-sandbox/hello:local
 sleep 10
 curl localhost:49160 | grep "Hello World"
 docker container stop hello
